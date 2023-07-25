c0a3f118a78eacebd2c30cf5a4ea0a09b32eab34
Hugo Thimonier <hugothimonier@macbook-pro-these.home>
2023-07-25 11:24:21 +0200

    ADD: adding the .sh files to run experiments

---
+++ scripts/cpu/optdigits.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+conda activate nptad
+rm -rf ./data/optdigits/optdigits/*
+export Q_DATE=$(date "+%d_%m_%y_%H_%M") 
+mkdir -p ./logs/optdigits
+mkdir -p ./results/optdigits
+
+python -u run.py \
+    --n_runs 20 \
+    --project NPT-AD \
+    --data_path ./data/optdigits/ \
+    --data_set optdigits \
+    --res_dir job_29_03_2023__15_33_25_seed_1 \
+    --data_loader_nprocs 0 \
+    --data_set_on_cuda False \
+    --exp_name optdigits_params \
+    --np_seed 1 \
+    --torch_seed 1 \
+    --original_torch_seed 1 \
+    --exp_batch_size -1 \
+    --exp_val_batchsize 100 \
+    --exp_num_train_inference -1 \
+    --exp_num_total_steps 500 \
+    --exp_eval_every_n 500 \
+    --exp_eval_every_epoch_or_steps epochs \
+    --exp_optimizer lookahead_lamb \
+    --exp_lookahead_update_cadence 6 \
+    --exp_optimizer_warmup_proportion 0 \
+    --exp_optimizer_warmup_fixed_n_steps 100 \
+    --exp_lr 0.01 \
+    --exp_scheduler flat_and_anneal \
+    --exp_gradient_clipping 1.0 \
+    --exp_weight_decay 0 \
+    --exp_deterministic_masks True \
+    --exp_checkpoint_setting all_checkpoints \
+    --exp_ssl_epochs 0 \
+    --exp_normalize_ad_loss True \
+    --exp_cat_as_num_features False \
+    --exp_keep_categorical_features True \
+    --model_init_weights True \
+    --model_init_params 0.05 \
+    --model_init_params 0 \
+    --model_init_type xavier \
+    --mp_no_sync -1 \
+    --mp_bucket_cap_mb -1 \
+    --exp_aggregation max \
+    --model_class NPT \
+    --model_is_semi_supervised False \
+    --model_amp True \
+    --model_feature_type_embedding False \
+    --model_feature_index_embedding False \
+    --model_bert_augmentation True \
+    --model_bert_mask_percentage 0.9 \
+    --model_dim_hidden 16 \
+    --model_augmentation_bert_mask_prob "dict(train=0.2, val=0.25, test=0.15)" \
+    --model_embedding_layer_norm True \
+    --model_att_block_layer_norm True \
+    --model_layer_norm_eps 1e-12 \
+    --model_pre_layer_norm True \
+    --model_hidden_dropout_prob 0.1 \
+    --model_att_score_dropout_prob 0.1 \
+    --model_checkpoint_key job__$Q_DATE \
+    --model_num_heads 4 \
+    --model_sep_res_embed True \
+    --model_stacking_depth 4 \
+    --model_mix_heads True \
+    --model_rff_depth 1 \
+    --ad True \
+    --data_force_reload True > ./logs/optdigits/$Q_DATE.log
---
+++ scripts/kddrev.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+conda activate nptad
+rm -rf ./data/kddrev/kddrev/*
+export Q_DATE=$(date "+%d_%m_%y_%H_%M") 
+mkdir -p ./logs/kddrev
+mkdir -p ./results/kddrev
+
+torchrun --nnodes=2 --nproc_per_node=4 run.py \
+    --n_runs 10 \
+    --data_path ./data/kddrev/ \
+    --data_set kddrev \
+    --res_dir job_$Q_DATE\
+    --data_loader_nprocs 0 \
+    --data_set_on_cuda False \
+    --exp_name kddrev_params \
+    --np_seed 1 \
+    --torch_seed 1 \
+    --original_torch_seed 1 \
+    --exp_batch_size 4096 \
+    --exp_val_batchsize 1048 \
+    --exp_num_train_inference 4096 \
+    --exp_num_total_steps 1000000.0 \
+    --exp_eval_every_n 5000 \
+    --exp_eval_every_epoch_or_steps epochs \
+    --exp_optimizer lookahead_lamb \
+    --exp_lookahead_update_cadence 6 \
+    --exp_optimizer_warmup_proportion 0 \
+    --exp_optimizer_warmup_fixed_n_steps 10000 \
+    --exp_lr 0.001 \
+    --exp_scheduler flat_and_anneal \
+    --exp_gradient_clipping 0.5 \
+    --exp_weight_decay 0 \
+    --exp_deterministic_masks True \
+    --exp_n_hidden_features 2 \
+    --exp_checkpoint_setting all_checkpoints \
+    --exp_ssl_epochs 0 \
+    --exp_normalize_ad_loss False \
+    --exp_cat_as_num_features False \
+    --exp_keep_categorical_features True \
+    --model_init_weights True \
+    --model_init_params 0.05 \
+    --model_init_params 0 \
+    --model_init_type xavier \
+    --mp_distributed True \
+    --mp_nodes 2 \
+    --mp_gpus 8 \
+    --mp_nr -1 \
+    --mp_no_sync -1 \
+    --mp_bucket_cap_mb -1 \
+    --exp_aggregation sum \
+    --model_class NPT \
+    --model_is_semi_supervised False \
+    --model_amp True \
+    --model_feature_type_embedding True \
+    --model_feature_index_embedding True \
+    --model_bert_augmentation True \
+    --model_bert_mask_percentage 0.9 \
+    --model_dim_hidden 16 \
+    --model_augmentation_bert_mask_prob "dict(train=0.15, val=0.15, test=0.1)" \
+    --model_embedding_layer_norm True \
+    --model_att_block_layer_norm True \
+    --model_layer_norm_eps 1e-12 \
+    --model_pre_layer_norm True \
+    --model_hidden_dropout_prob 0.1 \
+    --model_att_score_dropout_prob 0.1 \
+    --model_checkpoint_key job__$Q_DATE \
+    --model_num_heads 4 \
+    --model_sep_res_embed True \
+    --model_stacking_depth 4 \
+    --model_mix_heads True \
+    --model_rff_depth 1 \
+    --ad True \
+    --data_force_reload True  
